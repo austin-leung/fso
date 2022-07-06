d6b6c3227cf6165cd20185dcacfca33fcae39022
Austin Leung <austinhy@gmail.com>
2022-07-06 17:00:45 -0400

    finding notes

--- part3/notes/mongo.js
+++ part3/notes/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://austin:${password}@part4.485qk.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://austin:${password}@part4.485qk.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -19,19 +19,10 @@ const Note = mongoose.model('Note', noteSchema)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log('connected')
-
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
     })
-
-    return note.save()
-  })
-  .then(() => {
-    console.log('note saved!')
-    return mongoose.connection.close()
+    mongoose.connection.close()
   })
   .catch((err) => console.log(err))
