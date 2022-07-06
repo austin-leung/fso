822d5815cc68ca3b8b9d248a12849d807f9b22fb
Austin Leung <austinhy@gmail.com>
2022-07-06 19:41:40 -0400

    finish 3.18

--- fso_part3_phonebook/index.js
+++ fso_part3_phonebook/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+require('dotenv').config()
 
-app.use(express.json())
+
 const cors = require('cors')
+const PhonebookEntry = require('./models/persons')
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
 
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
@@ -21,54 +33,34 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/phonebook', (request, response) => {
-    response.json(phonebook)
+    PhonebookEntry.find({}).then(pb => {
+        response.json(pb)
+    })
 })
-
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${phonebook.length} people<br><br>${Date()}`)
 })
 
-app.get('/api/phonebook/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const pbEntry = phonebook.find(pb => pb.id === id)
-
-    if (pbEntry) {
-        response.json(pbEntry)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/phonebook/:id', (request, response, next) => {
+    PhonebookEntry.findById(request.params.id).then(pbEntry => {
+        if (pbEntry) {
+            response.json(pbEntry.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 
 app.delete('/api/phonebook/:id', (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(pb => pb.id !== id)
-    
-    response.status(204).end()
+    const id = request.params.id
+    PhonebookEntry.findByIdAndRemove(id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 function getRandomInt(max) {
@@ -90,26 +82,64 @@ app.post('/api/phonebook', (request, response) => {
         })
     }
 
-    const pbEntry = phonebook.find(pb => pb.name === body.name)
+    // const pbEntry = phonebook.find(pb => pb.name === body.name)
 
-    if (pbEntry) {
-        return response.status(400).json({
-            error: "name exist"
-        })
-    }
+    // if (pbEntry) {
+    //     return response.status(400).json({
+    //         error: "name exist"
+    //     })
+    // }
 
-    const pb = {
+    const pb = new PhonebookEntry({
         id: getRandomInt(100000),
         name: body.name || "no name",
         number: body.number || "no number"
-    }
+    })
 
-    phonebook = phonebook.concat(pb)
-
-    response.json(pb)
+    pb.save().then(savedPb => {
+        response.json(savedPb)
+    })
 })
+
+app.put('/api/phonebook/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+    const pb = {
+        name: body.name || "no name",
+        number: body.number || "no number"
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, pb, { new: true })
+      .then(updatedPb => {
+        response.json(updatedPb)
+      })
+      .catch(error => next(error))
+  })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
