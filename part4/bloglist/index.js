26f963e6add19cd788f3c5cfc5f442eecf8e77fd
Austin Leung <austinhy@gmail.com>
2022-07-07 13:37:40 -0400

    utils

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
@@ -38,7 +40,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
