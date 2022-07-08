1055e37cddad2c803c21992b26aed09cfbea01c2
Austin Leung <austinhy@gmail.com>
2022-07-08 15:19:36 -0400

    4.8 all blog test

--- part4/bloglist/tests/list.test.js
+++ part4/bloglist/tests/list.test.js
@@ -1,4 +1,11 @@
-const listHelper = require('../utils/list_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const listHelper = require('./utils/list_helper')
+const Blog = require('../models/blog')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -78,5 +85,43 @@ describe('total likes', () => {
   test('when list has multiple blogs, equals the likes of that', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('blog list tests', () => {
+  const initialBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await  blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+
+  test('returns correct amount of blog posts in JSON', async () => {
+    const response = await api.get('/api/blogs').expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body[0].title).toBe('React patterns')
   })
 })
