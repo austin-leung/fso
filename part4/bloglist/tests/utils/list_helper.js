7fefcb94abf08c70de01f3af7407354b5f881a2e
Austin Leung <austinhy@gmail.com>
2022-07-08 14:57:30 -0400

    4.4

--- part4/bloglist/tests/utils/list_helper.js
+++ part4/bloglist/tests/utils/list_helper.js
@@ -1,7 +1,14 @@
+const blogsRouter = require("../../controllers/blogs");
+
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((accum,item) => accum + item.likes, 0)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
