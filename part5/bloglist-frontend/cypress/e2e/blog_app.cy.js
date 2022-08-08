fa9ee6148a315974fa3ce126eadd9b528654a5a2
Austin Leung <austinhy@gmail.com>
2022-08-08 19:00:29 -0400

    5.18: failed login test

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -12,10 +12,25 @@ describe('Blog app', function() {
 
   it('Login form is shown', function() {
     cy.contains('login').click()
-    cy.get('#username').type('bikinibigboy')
-    cy.get('#password').type('secret')
-    cy.get('#login-button').click()
+  })
 
-    cy.contains('Spongebob Squarepants logged in')
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.contains('login').click()
+      cy.get('#username').type('bikinibigboy')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('Spongebob Squarepants logged in')
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.contains('login').click()
+      cy.get('#username').type('bikinibigboy')
+      cy.get('#password').type('plankton')
+      cy.get('#login-button').click()
+
+      cy.get('.error').should('contain', 'Wrong credentials')
+    })
   })
 })
