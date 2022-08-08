6a3ed4f71a48a8205ac0ace99c8db57def189704
Austin Leung <austinhy@gmail.com>
2022-08-08 16:49:21 -0400

    5.14 display blog url/likes on toggle

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('Blog', () => {
+  let component
   const blog = {
     _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
@@ -12,10 +14,40 @@ test('renders content', () => {
     likes: 7,
     __v: 0
   }
-  const { container } = render(<Blog blog={blog} />)
-  expect(container).toHaveTextContent('React patterns')
-  expect(container).toHaveTextContent('Michael Chan')
+  const user= {
+    username: 'bikinibigboy',
+    name: 'spongebob'
+  }
+  const mockHandlerRemove = jest.fn()
+  const mockHandlerUpdate = jest.fn()
 
-  const blogDetailDiv = screen.getByTestId('blog-detail')
-  expect(blogDetailDiv).toHaveStyle('display: none')
+  beforeEach(() => {
+    component = render(<Blog blog={blog} updateBlog={mockHandlerUpdate} deleteBlog={mockHandlerRemove} user={user} />)
+  })
+
+  test('renders content without likes/url', () => {
+    const { container } = component
+    expect(container).toHaveTextContent('React patterns')
+    expect(container).toHaveTextContent('Michael Chan')
+
+    const blogDetailDiv = screen.getByTestId('blog-detail')
+    expect(blogDetailDiv).toHaveStyle('display: none')
+  })
+
+  test('renders content with likes/url', async () => {
+    const { container } = component
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(container).toHaveTextContent('React patterns')
+    expect(container).toHaveTextContent('Michael Chan')
+    expect(container).toHaveTextContent('Likes')
+    expect(container).toHaveTextContent(blog.likes)
+    expect(container).toHaveTextContent(blog.url)
+
+    const blogDetailDiv = screen.getByTestId('blog-detail')
+    expect(blogDetailDiv).not.toHaveStyle('display: none')
+  })
 })
